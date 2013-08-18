ea46cf9fbcc8b3cd5ef6541f26db99627b60d071
Benjamin Graner <graner@gmail.com>
2013-08-18 00:45:07 -0400

    i) Update StoryCtrl to use angularFire service which will automatically watch for changes on the objects in an array and sync with Firebase ii) Refactored to make each individual step in a story have its own image

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,6 +20,10 @@ application.controller('MainCtrl', ["$scope", "$rootScope", "angularFireCollecti
           });
   	};
 
+    $scope.removeStory = function(story) {
+      $scope.stories.splice($scope.stories.indexOf(story), 1);
+    };
+
   	// Listen to the 'login' events, capture the user on this controller's scope
   	$scope.$on("angularFireAuth:login", function(evt, user) {
   		// User logged in.
@@ -29,108 +33,136 @@ application.controller('MainCtrl', ["$scope", "$rootScope", "angularFireCollecti
   		$scope.$apply();
     });
 
-  	// Listen to 'logout' events, remove the user from this controller's scope
-	$scope.$on("angularFireAuth:logout", function(evt) {
-		// User logged out.
-		$scope.user = null;
+    	// Listen to 'logout' events, remove the user from this controller's scope
+  	$scope.$on("angularFireAuth:logout", function(evt) {
+  		// User logged out.
+  		$scope.user = null;
 
-		// Force this controller to reapply the $scope (as user has now logged out)
-		$scope.$apply();
-	});
+  		// Force this controller to reapply the $scope (as user has now logged out)
+  		$scope.$apply();
+  	});
 
-	// Listen to error's logging in, don't know what to do with it though
-	$scope.$on("angularFireAuth:error", function(evt, err) {
-		// There was an error during authentication.
-	});
+  	// Listen to error's logging in, don't know what to do with it though
+  	$scope.$on("angularFireAuth:error", function(evt, err) {
+  		// There was an error during authentication.
+  	});
 }]);
 
-application.controller('StoryCtrl', ["$scope", "$rootScope", "$routeParams", "angularFireCollection", "angularFireAuth", function($scope, $rootScope, $routeParams, angularFireCollection, angularFireAuth) {
+application.controller('StoryCtrl', ["$scope", "$rootScope", "$routeParams", "angularFire", "angularFireCollection", "angularFireAuth", function($scope, $rootScope, $routeParams, angularFire, angularFireCollection, angularFireAuth) {
     
     $scope.storyId = $routeParams.storyId;
 
+    // Current story reference
     var url = "https://kerbalspacestories.firebaseio.com/stories/" + $scope.storyId;
     var ref = new Firebase(url);
 
-    var imgFireLocation = new Firebase(url + "/image/" + $scope.storyId);
-    imgFireLocation.once('value', function(snap) {
-      var payload = snap.val();
-      console.log("Data at " + imgFireLocation.toString() + ": " + payload)
-      $scope.imgSrc = payload;
-    });
+    // Step reference and collection
+    var storyStepsUrl = url + "/steps/";
 
-    ref.on('value', function(snapshot) {
-          $scope.storyDetails = snapshot.val().storyDetails;
-    });
+    angularFire(storyStepsUrl, $scope, "storySteps", []).then( function() {
+      
+      //var storyStepsRef = new Firebase(storyStepsUrl);
+      //$scope.storySteps = angularFireCollection(storyStepsRef);
 
-    $scope.file_changed = function(element, scope) {
+      // Image Firebase reference
+      // var imgFireLocation = new Firebase(url + "/image/" + $scope.storyId);
+      // imgFireLocation.once('value', function(snap) {
+      //   var payload = snap.val();
+      //   $scope.imgSrc = payload;
+      // });
 
-         $scope.$apply( function(scope) {
-             var photofile = element.files[0];
-             var reader = new FileReader();
-             reader.onload = function(e) {
-                console.log("This is where we would upload the file");
+      // ref.on('value', function(snapshot) {
+      //       $scope.storyDetails = snapshot.val().storyDetails;
+      // });
 
-                var payload = e.target.result;
-                console.log(payload);
+      $scope.file_changed = function(element, step, scope) {
+           $scope.$apply( function(scope) {
+               var photofile = element.files[0];
+               var reader = new FileReader();
+               reader.onload = function(e) {
+                  var payload = e.target.result;
+                  step.imgSrc = payload;
 
-                imgFireLocation.set(payload, function() {
-                  $scope.$apply(function() {
-                    $scope.imgSrc = payload;
-                  });
-                });
-             };
-             reader.readAsDataURL(photofile);
-         });
-    };
+                   var scopeStep = scope.storySteps[scope.storySteps.indexOf(step)];
 
-    // $scope.handleFileSelect = function(evt) {
-    //   var f = evt.target.files[0];
-    //   var reader = new FileReader();
-    //   reader.onload = (function(theFile) {
-    //     return function(e) {
-    //       var filePayload = e.target.result;
-    //       // Generate a location that can't be guessed using the file's contents and a random number
-    //       var hash = CryptoJS.SHA256(Math.random() + CryptoJS.SHA256(filePayload));
-    //       var f = new Firebase(firebaseRef + 'pano/' + hash + '/filePayload');
-    //       spinner.spin(document.getElementById('spin'));
-    //       // Set the file payload to Firebase and register an onComplete handler to stop the spinner and show the preview
-    //       f.set(filePayload, function() { 
-    //         spinner.stop();
-    //         document.getElementById("pano").src = e.target.result;
-    //         $('#file-upload').hide();
-    //         // Update the location bar so the URL can be shared with others
-    //         window.location.hash = hash;
-    //       });
-    //     };
-    //   })(f);
-    //   reader.readAsDataURL(f);
-    // }
+                   $scope.$apply(function() {
+                      scopeStep.imgSrc = payload;
+                   });
+                   
 
-    //$scope.stories = angularFireCollection(ref.limit(10));
+                  // var imgFireLocation = new Firebase(storyStepsUrl + step.name + "/image/");
+                  // imgFireLocation.set(payload, function() {
+                  //   $scope.$apply(function() {
+                  //     element.imgSrc = payload;
+                  //   });
+                  // });
+               };
+               reader.readAsDataURL(photofile);
+           });
+      };
 
-  	// Listen to the 'login' events, capture the user on this controller's scope
-	$scope.$on("angularFireAuth:login", function(evt, user) {
-  		// User logged in.
-  		$scope.user = user;
+      $scope.removeStep = function(step) {
+        $scope.storySteps.splice($scope.storySteps.indexOf(step), 1);
+      };
 
-  		// Force this controller to reapply the $scope (as we now have the currently logged in user object)
+          // Add a step
+      $scope.addStep = function() {
+        // Check that we have a user object - create a new story if we do otherwise display an error message
+        // $scope.storySteps.add({
+        //       name: $scope.stepName, userId: $scope.user.id
+        // });
+        $scope.storySteps.push({name: $scope.stepName, userId: $scope.user.id})
+      };
+
+      // $scope.handleFileSelect = function(evt) {
+      //   var f = evt.target.files[0];
+      //   var reader = new FileReader();
+      //   reader.onload = (function(theFile) {
+      //     return function(e) {
+      //       var filePayload = e.target.result;
+      //       // Generate a location that can't be guessed using the file's contents and a random number
+      //       var hash = CryptoJS.SHA256(Math.random() + CryptoJS.SHA256(filePayload));
+      //       var f = new Firebase(firebaseRef + 'pano/' + hash + '/filePayload');
+      //       spinner.spin(document.getElementById('spin'));
+      //       // Set the file payload to Firebase and register an onComplete handler to stop the spinner and show the preview
+      //       f.set(filePayload, function() { 
+      //         spinner.stop();
+      //         document.getElementById("pano").src = e.target.result;
+      //         $('#file-upload').hide();
+      //         // Update the location bar so the URL can be shared with others
+      //         window.location.hash = hash;
+      //       });
+      //     };
+      //   })(f);
+      //   reader.readAsDataURL(f);
+      // }
+
+      //$scope.stories = angularFireCollection(ref.limit(10));
+
+    	// Listen to the 'login' events, capture the user on this controller's scope
+  	$scope.$on("angularFireAuth:login", function(evt, user) {
+    		// User logged in.
+    		$scope.user = user;
+
+    		// Force this controller to reapply the $scope (as we now have the currently logged in user object)
+    		$scope.$apply();
+      });
+
+    	// Listen to 'logout' events, remove the user from this controller's scope
+  	$scope.$on("angularFireAuth:logout", function(evt) {
+  		// User logged out.
+  		$scope.user = null;
+
+  		// Force this controller to reapply the $scope (as user has now logged out)
   		$scope.$apply();
-    });
+  	});
 
-  	// Listen to 'logout' events, remove the user from this controller's scope
-	$scope.$on("angularFireAuth:logout", function(evt) {
-		// User logged out.
-		$scope.user = null;
-
-		// Force this controller to reapply the $scope (as user has now logged out)
-		$scope.$apply();
-	});
-
-	// Listen to error's logging in, don't know what to do with it though
-	$scope.$on("angularFireAuth:error", function(evt, err) {
-		// There was an error during authentication.
-		console.log("Error logging in");
-	});
+  	// Listen to error's logging in, don't know what to do with it though
+  	$scope.$on("angularFireAuth:error", function(evt, err) {
+  		// There was an error during authentication.
+  		console.log("Error logging in");
+  	});
+  });
 }]);
 
  application.controller('LoginController', ['$scope', '$rootScope', '$location', 'angularFireAuth', function($scope, $rootScope, $location, angularFireAuth) {
